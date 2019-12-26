c8f9991c486db71437de1cb40b1b67bab0134932
Firas Khalil Khana <firasuke@gmail.com>
2019-12-26 21:10:20 +0200

    Initial commit

---
+++ lib/gcc/x86_64-pc-linux-musl/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/glaucus/temporary/toolchain/sources/gcc/configure --build=x86_64-pc-linux-musl --host=x86_64-pc-linux-musl --target=x86_64-pc-linux-musl --prefix=/toolchain --with-local-prefix=/toolchain --with-native-system-header-dir=/toolchain/include --disable-multilib --enable-threads=posix --with-arch=x86-64 --disable-bootstrap --enable-languages=c,c++ --disable-libsanitizer --disable-libssp --disable-werror --disable-nls --with-linker-hash-style=gnu";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
