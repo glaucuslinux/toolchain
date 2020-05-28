82e69bff2d9f84cdaadb43a999b2eac2f2c1478a
Firas Khalil Khana <firasuke@gmail.com>
2020-05-28 16:37:40 +0300

    Revamp toolchain with final release 28052020

---
+++ lib/lib/gcc/x86_64-glaucus-linux-musl/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/firasuke/Downloads/Git/glaucus/temporary/toolchain/sources/gcc/configure --host=x86_64-glaucus-linux-musl --target=x86_64-glaucus-linux-musl --prefix=/toolchain --libexecdir=/toolchain/lib --libdir=/toolchain/lib --with-local-prefix=/toolchain --with-native-system-header-dir=/toolchain/include --disable-multilib --disable-symvers --disable-cet --enable-threads=posix --with-arch=x86-64 --with-tune=generic --disable-bootstrap --enable-languages=c,c++ --disable-libsanitizer --disable-libssp --disable-werror --disable-nls --disable-rpath --enable-libstdcxx-time=rt --with-pkgversion='glaucus x86-64' --with-bugurl=https://github.com/glaucuslinux/glaucus/issues --with-zstd --without-included-gettext --with-system-zlib --with-linker-hash-style=gnu";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "generic" } };
