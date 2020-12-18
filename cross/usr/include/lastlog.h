d6c0cf18aaa3c45fea40fe23d08b3f90a5a1d04d
Firas Khalil Khana <firasuke@gmail.com>
2020-12-18 23:45:57 +0200

    New toolchain release 18122020

--- cross/usr/include/lastlog.h
+++ cross/usr/include/lastlog.h
@@ -1,1 +1,23 @@
-#include <utmp.h>
+#ifndef _LASTLOG_H
+#define _LASTLOG_H
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define UT_HOSTSIZE 256
+#define UT_LINESIZE 32
+
+struct lastlog {
+	time_t ll_time;
+	char ll_line[UT_LINESIZE];
+	char ll_host[UT_HOSTSIZE];
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
